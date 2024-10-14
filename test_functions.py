985ca48afb48c39da787a6919eebeafa9d7052e4
tanmaysingha33 <166908298+tanmaysingha33@users.noreply.github.com>
2024-10-14 16:40:48 +0800

    Update test_functions.py

--- test_functions.py
+++ test_functions.py
@@ -15,5 +15,6 @@ def test_subtract():
 def test_convert_fahrenheit_to_celsius():
     assert f2c(32) == 0
     assert f2c(122) == pytest.approx(50)
-    with pytest.raises(AssertionError):
+    with pytest.raises(ValueError):
         f2c(-600)
+
